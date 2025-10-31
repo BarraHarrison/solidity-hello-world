edbcb5a4cf5387d8987093ff64699b77649a78c0
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-31 10:32:59 +0900

    Setting up a Sepolia URL and Key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,8 +17,8 @@ const config: HardhatUserConfig = {
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
   },
 };
