a9b709b5e4834deb3b92eb2f082f71532c0c9ea1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-31 10:56:21 +0900

    Defining the url and accounts to avoid a typescript error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,20 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
+if (!SEPOLIA_RPC_URL) {
+  throw new Error("Please define SEPOLIA_RPC_URL in your .env file");
+}
+
+if (!SEPOLIA_PRIVATE_KEY) {
+  throw new Error("Please define SEPOLIA_PRIVATE_KEY in your .env file");
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -17,8 +31,8 @@ const config: HardhatUserConfig = {
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
 };
