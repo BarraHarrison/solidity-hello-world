50fb4f56478405affd6b1d026db993ba93ec9a90
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-02 11:58:23 +0900

    Simplyfying the hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,10 @@
-import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-ethers";
-import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-
-dotenv.config();
-
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
-
-if (!SEPOLIA_RPC_URL) {
-  throw new Error("Please define SEPOLIA_RPC_URL in your .env file");
-}
-
-if (!SEPOLIA_PRIVATE_KEY) {
-  throw new Error("Please define SEPOLIA_PRIVATE_KEY in your .env file");
-}
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.28",
-    settings: {
-      optimizer: { enabled: true, runs: 200 },
-    },
-  },
+  solidity: "0.8.28",
   networks: {
-    hardhat: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: SEPOLIA_RPC_URL,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
+    hardhat: {},
   },
 };
 
